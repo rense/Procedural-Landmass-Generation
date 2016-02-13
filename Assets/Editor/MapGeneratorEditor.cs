fc3372de73c4436d665ba4df75c3c3cb4bbb6aa1
Rense VanderHoek <rense@me.com>
2016-02-14 00:27:58 +0100

    After E04: Colours

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor {
+
+	public override void OnInspectorGUI() {
+		MapGenerator mapGenerator = (MapGenerator)target;
+
+		if(DrawDefaultInspector()) {
+			if(mapGenerator.autoUpdate) {
+				mapGenerator.GenerateMap();
+			}
+		}
+
+		if(GUILayout.Button("Generate")) {
+			mapGenerator.GenerateMap();
+		}
+
+	}
+}
