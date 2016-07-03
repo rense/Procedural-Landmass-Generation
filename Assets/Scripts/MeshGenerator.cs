bd30dffbeb7bd189a4fa400b251438d102e41d7f
Rense VanderHoek <rense@me.com>
2016-07-03 11:07:30 +0200

    E12 Normals - own RecalculateNormals method

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -54,9 +54,7 @@ public class MeshData {
 	public MeshData(int meshWidth, int meshHeight) {
 		vertices = new Vector3[meshWidth * meshHeight];
 		uvs = new Vector2[meshWidth * meshHeight];
-
 		triangles = new int[(meshWidth - 1) * (meshHeight - 1) * 6];
-
 	}
 
 	public void AddTriangle(int a, int b, int c) {
@@ -66,12 +64,46 @@ public class MeshData {
 		triangleIndex += 3;
 	}
 
+	Vector3[] CalculateNormals() {
+		Vector3[] vertexNormals = new Vector3[vertices.Length];
+		int triangleCount = triangles.Length / 3;
+		for(int i = 0; i < triangleCount; i++) {
+			int normalTriangleIndex = i * 3;
+			int vertexIndexA = triangles[normalTriangleIndex];
+			int vertexIndexB = triangles[normalTriangleIndex + 1];
+			int vertexIndexC = triangles[normalTriangleIndex + 2];
+
+			Vector3 triangleNormal = SurfaceNormalFromIndices(vertexIndexA, vertexIndexB, vertexIndexC);
+			vertexNormals[vertexIndexA] += triangleNormal;
+			vertexNormals[vertexIndexB] += triangleNormal;
+			vertexNormals[vertexIndexC] += triangleNormal;
+		}
+		for(int i = 0; i < vertexNormals.Length; i++) {
+			vertexNormals[i].Normalize();
+		}
+
+		return vertexNormals;
+	}
+
+	Vector3 SurfaceNormalFromIndices(int indexA, int indexB, int indexC) {
+		Vector3 pointA = vertices[indexA];
+		Vector3 pointB = vertices[indexB];
+		Vector3 pointC = vertices[indexC];
+
+		Vector3 sideAB = pointB - pointA;
+		Vector3 sideAC = pointC - pointA;
+		return Vector3.Cross(sideAB, sideAC).normalized;
+	}
+
 	public Mesh CreateMesh() {
 		Mesh mesh = new Mesh();
 		mesh.vertices = vertices;
 		mesh.triangles = triangles;
 		mesh.uv = uvs;
-		mesh.RecalculateNormals();
+
+		// mesh.RecalculateNormals();
+		mesh.normals = CalculateNormals();
+
 		return mesh;
 
 	}
